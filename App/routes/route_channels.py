8cd8d16ab4aaea7fc4086069bc28a49e7de23d57
samuel <samu.17.rock@gmail.com>
2023-09-23 23:56:54 -0300

    corrigiendo error en la ruta canal

--- App/routes/route_channels.py
+++ App/routes/route_channels.py
@@ -1,11 +1,11 @@
 from flask import Blueprint
 from ..controllers.controller_channels import ChannelController
 
-bp_servers = Blueprint('channels', __name__)
+bp_channels = Blueprint('channels', __name__)
 
-bp_servers.route("/", method= ["GET"]) (ChannelController.get)
-bp_servers.route("/<int:channel_id>", method=["GET"])(ChannelController.get_id)
-bp_servers.route("/", method=["POST"])(ChannelController.create)
-bp_servers.route("/<int:channel_id>", method =["PUT"])(ChannelController.update)
-bp_servers.route("/<int:channel:id>", method= ["DELETE"])(ChannelController.delete)
-bp_servers.route("<int:channel_id>/messages",method=["GET"])(ChannelController.get_messages)
+bp_channels.route("/", method= ["GET"]) (ChannelController.get)
+bp_channels.route("/<int:channel_id>", method=["GET"])(ChannelController.get_id)
+bp_channels.route("/", method=["POST"])(ChannelController.create)
+bp_channels.route("/<int:channel_id>", method =["PUT"])(ChannelController.update)
+bp_channels.route("/<int:channel:id>", method= ["DELETE"])(ChannelController.delete)
+bp_channels.route("<int:channel_id>/messages",method=["GET"])(ChannelController.get_messages)
